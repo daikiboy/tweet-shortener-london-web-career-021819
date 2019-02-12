d035b68bfa234c89244e79f62574e49ec56d56a4
simone.rainieri <simone.rainieri@gmail.com>
2019-02-12 19:09:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,14 @@
-def dictionary
-  
+def dictionary(word)
+  word["hello"] = "hi"
+  word["to"] = "2"
+  word["two"] = "2"
+  word["too"] = "2"
+  word["for"] = "4"
+  word["four"] = "4"
+  word["be"] = "b"
+  word["you"] = "u"
+  word["at"] = "@"
+  word["and"] = "&"
 
 def word_substituer(tweetstring)
   # Write your code here.
