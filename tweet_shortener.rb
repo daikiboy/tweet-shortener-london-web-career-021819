36130b48cd71ca1bfad4060075f8a278bf01154d
simone.rainieri <simone.rainieri@gmail.com>
2019-02-12 19:17:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,14 @@
 def dictionary
   word{:hello=> "hi"}
   :word{:to=> "2"}
-  :word["two"] = "2"
-  :word["too"] = "2"
-  :word["for"] = "4"
-  :word["four"] = "4"
-  :word["be"] = "b"
-  :word["you"] = "u"
-  :word["at"] = "@"
-  :word["and"] = "&"
+  :word[:two=> "2"
+  :word[:too=>"2"
+  :word[:for=>"4"
+  :word[:four=>"4"
+  :word[:be=>"b"
+  :word[:you=>"u"
+  :word[:at=> "@"
+  :word[:and=> "&"
 end 
 
 def word_substituer(tweet)
